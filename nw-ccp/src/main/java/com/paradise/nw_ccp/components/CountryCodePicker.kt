6c7193c76a0b25eafe876f923a62e951344bcf95
nakwony <nvvyun@gmail.com>
2022-08-11 16:39:15 +0900

    fix: some code

--- nw-ccp/src/main/java/com/paradise/nw_ccp/components/CountryCodePicker.kt
+++ nw-ccp/src/main/java/com/paradise/nw_ccp/components/CountryCodePicker.kt
@@ -32,7 +32,7 @@ import com.paradise.nw_ccp.utils.getListCountries
 import java.util.*
 
 class CountryCodePicker {
-    private val currentCountryCode = mutableStateOf(Locale.getDefault().country.lowercase())
+    private val currentCountryCode = mutableStateOf("us")
 
     fun getCurrentCountryCode(): String {
         return currentCountryCode.value
